e638c65f4313ca4963307592f11613fab1a07379
S0l3k <turtleteamwcg@gmail.com>
2023-09-07 22:52:39 +0300

    add: styles for block new stickers, but don't work margin

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+var menu = ['1', '2', '3']
 const Stickers = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-  },
+      el: '.swiper-pagination',
+			clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+    },
 
   // Navigation arrows
   navigation: {
